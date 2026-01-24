35efd2dc5f5cd824fa14ad3b1ff5a1e66dccd8ec
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2026-01-23 19:21:44 -0800

    Align kernels implementation with ffmpeg
    See ffmpeg color yuv2rgb color conversion reference:
    * https://github.com/FFmpeg/FFmpeg/blob/a5d4c398b411a00ac09d8fe3b66117222323844c/libavfilter/opencl/colorspace_common.cl#L111C8-L111C15
    
    Color conversion matrix copied from the ffmpeg internal output of:
    
    ff_fill_rgb2yuv_table(av_csp_luma_coeffs_from_avcsp(color_space ), rgb2yuv);
    ff_matrix_invert_3x3(rgb2yuv, yuv2rgb);
    
    with color space AVCOL_PRI_BT470BG and AVCOL_PRI_BT709.
    
    Reference results for the nasa clip from torchcodec:
    
    ffmpeg -i nasa_13013.mp4 -vf "scale=480:270:sws_flags=bilinear,format=rgb24" -y nasa_13013_ffmpeg_cpu.rgb
    ffmpeg -f rawvideo -pix_fmt rgb24 -s:v 480x270 -i nasa_13013_xpu.rgb -f rawvideo -pix_fmt rgb24 -s:v 480x270 -i nasa_13013_ffmpeg_cpu.rgb -filter_complex "psnr"
    
    PSNR r:52.915639 g:48.554012 b:52.363531 average:50.815521 min:49.467900 max:51.852806
    
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- src/torchcodec_xpu/ColorConversionKernel.h
+++ src/torchcodec_xpu/ColorConversionKernel.h
@@ -7,17 +7,15 @@
 
 namespace facebook::torchcodec {
 
-// High-level conversion function for NV12 to RGB
-// Works with raw pointers for maximum flexibility
 void convertNV12ToRGB(
+    sycl::queue& queue,
     const uint8_t* y_plane,
     const uint8_t* uv_plane,
     uint8_t* rgb_output,
     int width,
     int height,
     int stride,
-    sycl::queue& queue,
-    int color_std = 1); // 0 = BT.601, 1 = BT.709
+    bool fullrange = 1);
 
 // Anchor function to force kernel registration
 void registerColorConversionKernel();
