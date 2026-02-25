dvrogozh/torchcodec-xpu

commit a60fcae30fa9e9151e1d9d2cd429754563f095dd

Switch to scikit-build-core and add dependencies
Switch to scikit-build-core is needed to support building with `uv` as working
with `uv` environments requires better handling of cmake configurations.

Adding `torch` and `torchcodec` to build and runtime dependencies simplifies
the build.

Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

pyproject.toml
@@ -1,6 +1,10 @@
 [build-system]
-requires = ["setuptools>=61.0"]
-build-backend = "setuptools.build_meta"
+requires = [
+    "scikit-build-core>=0.10",
+    "torch",
+    "torchcodec~=0.10.0"
+]
+build-backend = "scikit_build_core.build"
 
 [project]
 name = "torchcodec-xpu"
@@ -12,12 +16,17 @@ authors = [
     { name = "Dmitry Rogozhkin", email = "dmitry.v.rogozhkin@intel.com" },
 ]
 dynamic = ["version"]
+dependencies = [
+    "torchcodec~=0.10.0"
+]
 
 [project.urls]
 GitHub = "https://github.com/dvrogozh/torchcodec-xpu"
 
-[tool.setuptools.dynamic]
-version = {file = "version.txt"}
+[tool.scikit-build.metadata.version]
+provider = "scikit_build_core.metadata.regex"
+input = "version.txt"
+regex = "^v?(?P<value>[0-9a-zA-Z.+-_]+)"
 
 [project.optional-dependencies]
 test = [

commit 1d316840f2e862a80223607cf273289bf092923d

Fix library loading after switch to scikit-build-core
Switch to scikit-build-core requires changes in the extension loading
mechanism which I've overlooked.

Fixes: a60fcae ("Switch to scikit-build-core and add dependencies")
Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

pyproject.toml
@@ -23,6 +23,9 @@ dependencies = [
 [project.urls]
 GitHub = "https://github.com/dvrogozh/torchcodec-xpu"
 
+[tool.scikit-build]
+cmake.minimum-version = "3.18"
+
 [tool.scikit-build.metadata.version]
 provider = "scikit_build_core.metadata.regex"
 input = "version.txt"

setup.py
@@ -1,170 +1,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# Copyright (c) 2025 Dmitry Rogozhkin.
-
-import os
-import subprocess
-import sys
-from pathlib import Path
-
-import torch
-import torchcodec
-from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext
-
-_ROOT_DIR = Path(__file__).parent.resolve()
-
-
-class CMakeBuild(build_ext):
-
-    def __init__(self, *args, **kwargs):
-        self._install_prefix = None
-        super().__init__(*args, **kwargs)
-
-    def run(self):
-        try:
-            subprocess.check_output(["cmake", "--version"])
-        except OSError:
-            raise RuntimeError("CMake is not available.") from None
-        super().run()
-
-    def build_extension(self, ext):
-        """Call our CMake build system to build libtorchcodec*.so"""
-        # Setuptools was designed to build one extension (.so file) at a time,
-        # calling this method for each Extension object. We're using a
-        # CMake-based build where all our extensions are built together at once.
-        # If we were to declare one Extension object per .so file as in a
-        # standard setup, a) we'd have to keep the Extensions names in sync with
-        # the CMake targets, and b) we would be calling into CMake for every
-        # single extension: that's overkill and inefficient, since CMake builds
-        # all the extensions at once. To avoid all that we create a *single*
-        # fake Extension which triggers the CMake build only once.
-        assert ext.name == "FAKE_NAME", f"Unexpected extension name: {ext.name}"
-        # The price to pay for our non-standard setup is that we have to tell
-        # setuptools *where* those extensions are expected to be within the
-        # source tree (for sdists or editable installs) or within the wheel.
-        # Normally, setuptools relies on the extension's name to figure that
-        # out, e.g. an extension named `torchcodec.libtorchcodec.so` would be
-        # placed in `torchcodec/` and importable from `torchcodec.`. From that,
-        # setuptools knows how to move the extensions from their temp build
-        # directories back into the proper dir.
-        # Our fake extension's name is just a placeholder, so we have to handle
-        # that relocation logic ourselves.
-        # _install_prefix is the temp directory where the built extension(s)
-        # will be "installed" by CMake. Once they're copied to install_prefix,
-        # the built .so files still need to be copied back into:
-        # - the source tree (for editable installs) - this is handled in
-        #   copy_extensions_to_source()
-        # - the (temp) wheel directory (when building a wheel). I cannot tell
-        #   exactly *where* this is handled, but for this to work we must
-        #   prepend the "/torchcodec_xpu" folder to _install_prefix: this tells
-        #   setuptools to eventually move those .so files into `torchcodec_xpu/`.
-        # It may seem overkill to 'cmake install' the extensions in a temp
-        # directory and move them back to another dir, but this is what
-        # setuptools would do and expect even in a standard build setup.
-        self._install_prefix = (
-            Path(self.get_ext_fullpath(ext.name)).parent.absolute() / "torchcodec_xpu"
-        )
-        self._build_all_extensions_with_cmake()
-
-    def _build_all_extensions_with_cmake(self):
-        # Note that self.debug is True when you invoke setup.py like this:
-        # python setup.py build_ext --debug install
-        torch_dir = Path(torch.utils.cmake_prefix_path) / "Torch"
-        torchcodec_dir = Path(torchcodec.cmake_prefix_path) / "TorchCodec"
-        cmake_build_type = os.environ.get("CMAKE_BUILD_TYPE", "Release")
-        torchcodec_xpu_disable_compile_warning_as_error = os.environ.get(
-            "TORCHCODEC_XPU_DISABLE_COMPILE_WARNING_AS_ERROR", "OFF"
-        )
-        python_version = sys.version_info
-        cmake_args = [
-            f"-DCMAKE_INSTALL_PREFIX={self._install_prefix}",
-            f"-DTorch_DIR={torch_dir}",
-            f"-DTorchCodec_DIR={torchcodec_dir}",
-            "-DCMAKE_VERBOSE_MAKEFILE=ON",
-            f"-DCMAKE_BUILD_TYPE={cmake_build_type}",
-            f"-DPYTHON_VERSION={python_version.major}.{python_version.minor}",
-            f"-DTORCHCODEC_XPU_DISABLE_COMPILE_WARNING_AS_ERROR={torchcodec_xpu_disable_compile_warning_as_error}",
-        ]
-
-        self.build_temp = os.getenv("TORCHCODEC_XPU_CMAKE_BUILD_DIR", self.build_temp)
-        print(f"Using {self.build_temp = }", flush=True)
-        Path(self.build_temp).mkdir(parents=True, exist_ok=True)
-
-        print("Calling cmake (configure)", flush=True)
-        subprocess.check_call(
-            ["cmake", str(_ROOT_DIR)] + cmake_args, cwd=self.build_temp
-        )
-        print("Calling cmake --build", flush=True)
-        subprocess.check_call(["cmake", "--build", "."], cwd=self.build_temp)
-        print("Calling cmake --install", flush=True)
-        subprocess.check_call(["cmake", "--install", "."], cwd=self.build_temp)
-
-    def copy_extensions_to_source(self):
-        """Copy built extensions from temporary folder back into source tree.
-
-        This is called by setuptools at the end of .run() during editable installs.
-        """
-        self.get_finalized_command("build_py")
-        extensions = []
-        if sys.platform == "linux":
-            extensions = ["so"]
-        elif sys.platform == "darwin":
-            # Mac has BOTH .dylib and .so as library extensions. Short version
-            # is that a .dylib is a shared library that can be both dynamically
-            # loaded and depended on by other libraries; a .so can only be a
-            # dynamically loaded module. For more, see:
-            #   https://stackoverflow.com/a/2339910
-            extensions = ["dylib", "so"]
-        elif sys.platform in ("win32", "cygwin"):
-            extensions = ["dll"]
-        else:
-            raise NotImplementedError(f"Platform {sys.platform} is not supported")
-
-        for ext in extensions:
-            for lib_file in self._install_prefix.glob(f"*.{ext}"):
-                assert "libtorchcodec" in lib_file.name
-                destination = Path("src/torchcodec_xpu/") / lib_file.name
-                print(f"Copying {lib_file} to {destination}")
-                self.copy_file(lib_file, destination, level=self.verbose)
-
-
-# See `CMakeBuild.build_extension()`.
-fake_extension = Extension(name="FAKE_NAME", sources=[])
-
-
-def _write_version_files():
-    if version := os.getenv("BUILD_VERSION"):
-        # BUILD_VERSION is set by the `test-infra` build jobs. It typically is
-        # the content of `version.txt` plus some suffix like "+cpu" or "+cu112".
-        # See
-        # https://github.com/pytorch/test-infra/blob/61e6da7a6557152eb9879e461a26ad667c15f0fd/tools/pkg-helpers/pytorch_pkg_helpers/version.py#L113
-        version = version.replace("+cpu", "")
-        with open(_ROOT_DIR / "version.txt", "w") as f:
-            f.write(f"{version}")
-    else:
-        with open(_ROOT_DIR / "version.txt") as f:
-            version = f.readline().strip()
-        try:
-            version = version.replace("+cpu", "")
-            sha = (
-                subprocess.check_output(
-                    ["git", "rev-parse", "HEAD"], cwd=str(_ROOT_DIR)
-                )
-                .decode("ascii")
-                .strip()
-            )
-            version += "+" + sha[:7]
-        except Exception:
-            print("INFO: Didn't find sha. Is this a git repo?")
-
-    with open(_ROOT_DIR / "src/torchcodec_xpu/version.py", "w") as f:
-        f.write("# Note that this file is generated during install.\n")
-        f.write(f"__version__ = '{version}'\n")
-
-
-_write_version_files()
-
-setup(
-    ext_modules=[fake_extension],
-    cmdclass={"build_ext": CMakeBuild},
-)

src/torchcodec_xpu/CMakeLists.txt
@@ -1,7 +1,7 @@
 # Copyright (c) 2025 Dmitry Rogozhkin.
 
 cmake_minimum_required(VERSION 3.18)
-project(TorchCodec)
+project(${SKBUILD_PROJECT_NAME})
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
@@ -9,7 +9,7 @@ set(PYBIND11_FINDPYTHON ON)
 find_package(pybind11 REQUIRED)
 find_package(Torch REQUIRED)
 find_package(TorchCodec REQUIRED)
-find_package(Python3 ${PYTHON_VERSION} EXACT COMPONENTS Development)
+find_package(Python3 EXACT COMPONENTS Development)
 
 if(DEFINED TORCHCODEC_XPU_DISABLE_COMPILE_WARNING_AS_ERROR AND TORCHCODEC_XPU_DISABLE_COMPILE_WARNING_AS_ERROR)
     set(TORCHCODEC_XPU_WERROR_OPTION "")
@@ -33,11 +33,11 @@ pkg_check_modules(L0 REQUIRED IMPORTED_TARGET level-zero)
 pkg_check_modules(LIBVA REQUIRED IMPORTED_TARGET libva)
 
 function(make_torchcodec_xpu_libraries torchcodec_variant)
-    set(libname "libtorchcodec_xpu${torchcodec_variant}")
+    set(libname "xpu_ops${torchcodec_variant}")
     set(sources
         XpuDeviceInterface.cpp)
 
-    add_library(${libname} SHARED ${sources})
+    python_add_library(${libname} MODULE WITH_SOABI ${sources})
     # Avoid adding the "lib" prefix which we already add explicitly.
     set_target_properties(${libname} PROPERTIES PREFIX "")
     set_target_properties(${libname} PROPERTIES CXX_STANDARD 17)
@@ -45,36 +45,16 @@ function(make_torchcodec_xpu_libraries torchcodec_variant)
         PRIVATE
         "${Python3_INCLUDE_DIRS}"
     )
-    target_link_libraries(${libname}
+    target_link_libraries(${libname} PRIVATE
       ${TORCH_LIBRARIES}
       torchcodec::core${torchcodec_variant}
       torchcodec::ffmpeg${torchcodec_variant}
       PkgConfig::L0
     )
 
-    if(WIN32)
-        # On Windows, the built artifacts are put in Release/Debug
-        # subdirectories by default. We want to avoid that, otherwise our
-        # install() step would not know where to find those.
-        set_target_properties(${libname} PROPERTIES
-            RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_CURRENT_BINARY_DIR}
-            RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_CURRENT_BINARY_DIR}
-            LIBRARY_OUTPUT_DIRECTORY_DEBUG ${CMAKE_CURRENT_BINARY_DIR}
-            LIBRARY_OUTPUT_DIRECTORY_RELEASE ${CMAKE_CURRENT_BINARY_DIR}
-            ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${CMAKE_CURRENT_BINARY_DIR}
-            ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${CMAKE_CURRENT_BINARY_DIR}
-        )
-    endif()
-
-    # The install step is invoked within CMakeBuild.build_library() in
-    # setup.py and just copies the built files from the temp
-    # cmake/setuptools build folder into the CMAKE_INSTALL_PREFIX folder. We
-    # still need to manually pass "DESTINATION ..." for cmake to copy those
-    # files in CMAKE_INSTALL_PREFIX instead of CMAKE_INSTALL_PREFIX/lib.
     install(
         TARGETS ${libname}
-        LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}
-        RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}  # For Windows DLLs
+	DESTINATION ${SKBUILD_PROJECT_NAME}
     )
 endfunction()
 

src/torchcodec_xpu/__init__.py
@@ -3,43 +3,22 @@
 
 import ctypes
 import importlib
-import sys
 import traceback
-from pathlib import Path
 
 import torch
 import torchcodec
 
 
 def _get_extension_path(lib_name: str) -> str:
-    extension_suffixes = []
-    if sys.platform == "linux":
-        extension_suffixes = importlib.machinery.EXTENSION_SUFFIXES
-    #elif sys.platform in ("win32", "cygwin"):
-    #    extension_suffixes = importlib.machinery.EXTENSION_SUFFIXES + [".dll", ".pyd"]
-    else:
-        raise NotImplementedError(f"{sys.platform = } is not not supported")
-    loader_details = (
-        importlib.machinery.ExtensionFileLoader,
-        extension_suffixes,
-    )
-
-    extfinder = importlib.machinery.FileFinder(
-        str(Path(__file__).parent), loader_details
-    )
-    ext_specs = extfinder.find_spec(lib_name)
-    if ext_specs is None:
+    spec = importlib.util.find_spec(lib_name)
+    if spec is None or spec.origin is None:
         raise ImportError(f"No spec found for {lib_name}")
-
-    if ext_specs.origin is None:
-        raise ImportError(f"Existing spec found for {lib_name} does not have an origin")
-
-    return ext_specs.origin
+    return spec.origin
 
 def load_torchcodec_xpu_shared_library():
     exceptions = []
     ffmpeg_major_version = torchcodec.ffmpeg_major_version
-    xpu_library_name = f"libtorchcodec_xpu{ffmpeg_major_version}"
+    xpu_library_name = f"torchcodec_xpu.xpu_ops{ffmpeg_major_version}"
     try:
         ctypes.CDLL(torchcodec.core_library_path)
         torch.ops.load_library(_get_extension_path(xpu_library_name))